a58934f9b2d00cba2f1eb8326359d19dc4c1c119
Adil Khan <54857821+adilk0@users.noreply.github.com>
2021-08-09 02:45:28 +0530

    Update Section1.js

--- src/component/Section1.js
+++ src/component/Section1.js
@@ -33,10 +33,6 @@ export const Section1 = () => {
                         </div>
                     </div>
             </div>
-
-                <!-- TILT SCRIPT -->
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-                <script src="./JS/tilt.jquery.js"></script> <!-- Load Tilt.js library -->
         </div>
     )
 }
